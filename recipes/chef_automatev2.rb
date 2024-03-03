9c074af0c223471f9006ef99690f98c2d93fc21f
Corey Hemminger <hemminger@hotmail.com>
2024-03-02 19:37:37 -0600

    test

--- recipes/chef_automatev2.rb
+++ recipes/chef_automatev2.rb
@@ -22,21 +22,21 @@ chef_automatev2 'Create Automate server' do
   end
 end
 
-if node['chef_software']['automate_admin_token']
-  node['chef_software']['automatev2_local_users']&.each do |name, hash|
-    iam_user name do
-      user_hash hash['user_json']
-      api_token node['chef_software']['automate_admin_token']
-      action :create
-    end
+node['chef_software']['automatev2_local_users']&.each do |name, hash|
+  iam_user name do
+    user_hash hash['user_json']
+    api_token node['chef_software']['automate_admin_token']
+    action :create
+    only_if { node['chef_software']['automate_admin_token'] }
   end
+end
 
-  node['chef_software']['automatev2_iam_policies']&.each do |name, hash|
-    iam_policy name do
-      policy_hash hash['policy_json']
-      api_token node['chef_software']['automate_admin_token']
-      action :create
-    end
+node['chef_software']['automatev2_iam_policies']&.each do |name, hash|
+  iam_policy name do
+    policy_hash hash['policy_json']
+    api_token node['chef_software']['automate_admin_token']
+    action :create
+    only_if { node['chef_software']['automate_admin_token'] }
   end
 end
 
